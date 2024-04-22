de20e7885c4619a2ae9fd00c2f1a5c954d9baeb6
saileshbindu <sailesh.bindu189@gmail.com>
2024-04-22 17:36:24 +0530

    Navbar add nav

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import {Grid} from '@mui/material';
 import './navbar.css';
-import Search from '../search/search';
+import SearchBar from '../SearchBar/search';
 import { useTheme } from '@mui/material/styles';
-import Logo from '../logo/logo';
-import FeedbackBut from '../button/button';
+import Logo from '../Logo/logo';
+import Button from '../Button/button';
 
 
 const Navbar = () =>{
     const theme = useTheme();
     return(
        <div>
-        <Grid container className="navbarBg" sx={{display: theme.palette.display}}>
+        <Grid container component="nav" className="navbarBg" sx={{display: theme.palette.display}}>
         <Grid xs={3} >
             <Logo />
         </Grid>
         <Grid xs={6}>
-        <Search />
+        <SearchBar />
         </Grid>
         <Grid xs={3}>
-        <FeedbackBut />
+        <Button />
         </Grid>
         </Grid>
        </div>
